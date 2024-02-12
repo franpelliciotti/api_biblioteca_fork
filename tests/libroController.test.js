edd588dacfee79542323e8950f8af586d65a9b0e
franpeliciotti <franpelliciotti00@gmail.com>
2024-02-12 15:31:16 -0300

    Tests Unitarios y de Integración

--- tests/libroController.test.js
+++ tests/libroController.test.js
@@ -4,17 +4,19 @@ const {
     updateLibro,
     deleteLibro,
     getLibroById,
-    } = require("../../src/controllers/libroController");
+    } = require("./../src/controllers/libroController");
 
-const libroModel = require("../../src/models/libroModel.js");
-jest.mock("../../src/models/libroModel.js");
+const libroModel = require("./../src/models/libroModel.js");
+
+jest.mock("./../src/models/libroModel.js");
 
 describe("Libro Controller", () => {
         let mockRes;
+
         beforeEach(() => {
             mockRes = {
-            status: jest.fn().mockReturnThis(),
-            json: jest.fn(),
+                status: jest.fn().mockReturnThis(),
+                json: jest.fn(),
         }
     });
     });
@@ -24,9 +26,13 @@ test("getLibros debería obtener todos los libros", async () => {
             { id: "1", title: "Libro 1" },
             { id: "2", title: "Libro 2" },
         ];
+        
         libroModel.find.mockResolvedValue(mockLibros);
+        
         const mockReq = {};
+        
         await getAllLibros(mockReq, mockRes);
+
         expect(mockRes.status).toHaveBeenCalledWith(200);
         expect(mockRes.json).toHaveBeenCalledWith(mockLibros);
     });
@@ -34,9 +40,13 @@ test("getLibros debería obtener todos los libros", async () => {
 test("getLibroById debería obtener un libro", async () => {
     const mockLibro = { id: "1", titulo: "Libro Encontrado", autor:
     "Juan Perez" };
+
     libroModel.findById.mockResolvedValue(mockLibro);
+    
     const mockReq = { params: { id: "1" } };
+    
     await getLibroById(mockReq, mockRes);
+    
     expect(mockRes.status).toHaveBeenCalledWith(200);
     expect(mockRes.json).toHaveBeenCalledWith(mockLibro);
     });
@@ -44,9 +54,13 @@ test("getLibroById debería obtener un libro", async () => {
 test("createLibro debería crear un nuevo libro", async () => {
     const mockLibro = { id: "1", titulo: "Nuevo Libro", autor: "Juan Perez" };
     mockLibro.save = () => {};
+    
     libroModel.create.mockResolvedValue(mockLibro);
+    
     const mockReq = { body: mockLibro };
+    
     await createLibro(mockReq, mockRes);
+    
     expect(mockRes.status).toHaveBeenCalledWith(201);
     expect(mockRes.json).toHaveBeenCalledWith(mockLibro);
     });
@@ -57,9 +71,13 @@ test("updateLibro debería actualizar un libro existente", async () =>
     const libroActualizado = { titulo: 'Libro Actualizado', autor:
     'Autor Actualizado' };
     const libroActualizadoMock = { _id: libroId, ...libroActualizado };
+    
     libroModel.findByIdAndUpdate.mockResolvedValue(libroActualizadoMock);
+    
     const mockReq = { params: { id: "1" }, body: libroActualizado };
+    
     await updateLibro(mockReq, mockRes);
+    
     expect(libroModel.findByIdAndUpdate).toHaveBeenCalledWith(libroId,
     libroActualizado, { new: true });
     expect(mockRes.status).toHaveBeenCalledWith(200);
@@ -67,22 +85,30 @@ test("updateLibro debería actualizar un libro existente", async () =>
     });
     
 test("updateLibro debería devolver un error si el libro no existe", async () => {
+    
     libroModel.findByIdAndUpdate.mockResolvedValue(null);
+    
     const mockReq = {
         params: { id: "99" },
         body: { titulo: "Libro Actualizado" },
     };
+    
     await updateLibro(mockReq, mockRes);
+    
     expect(mockRes.status).toHaveBeenCalledWith(404);
     expect(mockRes.json).toHaveBeenCalledWith({ error: "Libro no encontrado" });
     });
     
 test("deleteLibro debería eliminar un libro existente", async () => {
     const mockLibroEliminado = { titulo: 'Libro Eliminado', autor: 'Autor Eliminado' };
+    
     libroModel.findByIdAndRemove.mockResolvedValue(mockLibroEliminado);
+    
     const mockReq = { params: { id: "1" } };
+    
     await deleteLibro(mockReq, mockRes);
-    expect(libroModel.findByIdAndRemove).toHaveBeenCalledWith(mockReq.params.id);
+    
+expect(libroModel.findByIdAndRemove).toHaveBeenCalledWith(mockReq.params.id);
     expect(mockRes.status).toHaveBeenCalledWith(200);
     expect(mockRes.json).toHaveBeenCalledWith(mockLibroEliminado);
     });
